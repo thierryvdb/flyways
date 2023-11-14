868244df228350997058c4689664c928d4e82914
Thierry Vanden Broucke <thierry.p.broucke@altice.pt>
2023-11-14 12:36:35 -0300

    Adding new table

---
+++ Coffee/migrations/V003_20231113212832__cloud.sql
@@ -0,0 +1,9 @@
+CREATE TABLE astronauts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    birth_date DATE,
+    nationality VARCHAR(50),
+    spaceflights INT,
+    mission_specialty VARCHAR(100)
+);
