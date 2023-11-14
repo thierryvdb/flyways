a454529e7da6b8410dab53a8e57c3ef86a937ab1
Thierry <48493599+thierryvdb@users.noreply.github.com>
2023-11-14 11:07:56 -0300

    Create V002_20231113212832__cloud.sql

---
+++ Coffee/migrations/V002_20231113212832__cloud.sql
@@ -0,0 +1,7 @@
+CREATE TABLE teacher (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    subject VARCHAR(100),
+    hire_date DATE
+);
